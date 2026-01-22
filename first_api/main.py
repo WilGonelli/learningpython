af18f495fedeeb621e13b886117c2faf50330df3
William Gonelli <William.Vieira@fit-tecnologia.org.br>
2026-01-22 10:04:53 -0300

    atualizado primeiro projeto usando fastapi com uma explicação sobre middlewares e dependencies

--- first_api/main.py
+++ first_api/main.py
@@ -4,7 +4,7 @@
 #                   fastapi  |  condiçoes para a variavel
 #                       |    |        |
 #                       V    |        V
-from fastapi import FastAPI,        Query
+from fastapi import FastAPI,        Query,                      Body,           Request,        Depends, Header, HTTPException
 
 # importação da lib pydantic - biblioteca para validação de tipos
 # esse biblioteca valida os dados e caso seja necessario tenta converter lançando um erro caso ocorra
@@ -14,8 +14,57 @@ from pydantic import BaseModel
 # importação da lib para validação de tipo
 from typing import Annotated
 
+
+import time
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+
 # instancia do fastAPI
 app = FastAPI()
+
+# o cors dita quem pode se conectar com a aplicação, ou seja, que local da requisição sera permitido a conexão
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # <-- lista de locals aceito para a conexao
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# o middleware é uma função que é executada antes das funções das rotas
+# quando definimos um middleware todas as rotas passam por essa função
+# caso seja necessario uma função antes de algumas rotas e não obrigatoriamente em todas, podemos usar dependecies nas rotas
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+# dessa forma com dependencies, vocce pode criar middlewares que serao executados apenas nas rotas escolhidas
+async def verificar_token(x_token: str = Header()):
+    if x_token != "token-secreto":
+        raise HTTPException(status_code=400, detail="Token inválido")
+
+@app.get("/protegido", dependencies=[Depends(verificar_token)])
+async def rota_protegida():
+    return {"msg": "Você tem acesso!"}
+
+@app.get("/publico") # Esta rota NÃO executa a verificação
+async def rota_publica():
+    return {"msg": "Livre para todos"}
+
+
 
 # classe do body esperado pela requisição
 # usado o pydantic para a validação dos tipos
@@ -95,6 +144,71 @@ async def read_items(
     if q:
         results.update({"q": q})
     return results
+
+@app.put("/items6/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        # podemos adicionar exemplo do que esperamos na requisição para ser apresentado no docs
+        Body(
+            examples=[
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ],
+        ),
+    ],
+):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/items7/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        # podemos ter mais de um exemplo assim como exemplos de requisição certa e errada
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
 
 if __name__ == "__main__":
     import uvicorn
